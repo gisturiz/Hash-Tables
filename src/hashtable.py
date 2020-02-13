5c6786acac004e2c48915f93ad8f7c39d3f1e6f8
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-02-13 10:26:07 -0500

    finished hash table set up

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,24 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -24,14 +27,12 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
-
 
     def _hash_mod(self, key):
         '''
@@ -41,28 +42,35 @@ class HashTable:
 
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
         '''
         Store the value with the given key.
         Hash collisions should be handled with Linked List Chaining.
         Fill this in.
         '''
+        # hash the key
         key_hash = self._hash_mod(key)
+
+        # create link pair node variable
         new_node = LinkedPair(key, value)
 
+        # if storage at index is None, insert key, value at index
         if self.storage[key_hash] is None:
             self.storage[key_hash] = new_node
+        # elif if key already exist, update value
+        elif self.storage[key_hash].key == key:
+            self.storage[key_hash].value = value
         else:
-            self.storage[key_hash] = new_node.next
-            print(f"Warning: Collision has ocurred at {key_hash}")
-            return
-            # for pair in self.storage:
-            #     if pair[0] == key:
-            #         pair[1] = value
-            # self.storage[key_hash].append(key_value)
-
-
+            # go to index on storage
+            last_item = self.storage[key_hash]
+            # walk through LL and check for the last item or key match
+            while last_item.next is not None and last_item.key != key:
+                last_item = last_item.next
+            # if key matches, update value
+            if last_item.key == key:
+                last_item.value = value
+            # otherwise, create new node at end of LL
+            last_item.next = new_node
 
     def remove(self, key):
         '''
@@ -70,19 +78,26 @@ class HashTable:
         Print a warning if the key is not found.
         Fill this in.
         '''
+
+        # hash index key
         key_hash = self._hash_mod(key)
+        item = self.storage[key_hash]
 
-        if self.storage[key_hash] is None:
-            print("Warning: key not found.")
-        # for i in range(0, len(self.storage[key_hash])):
-        #     if self.storage[key_hash][i][0] == key:
-        #         self.storage[key_hash].pop(i)
-        else:
-            if self.storage[key_hash][0] == key:
-                self.storage[key_hash] = None
+        # if index not empty
+        if item is not None:
+            # and key matches
+            if item.key == key:
+                # assign the value to None
+                item.value = None
             else:
-                print("Warning: collision")
-
+                cur_item = item.next
+                while cur_item is not None:
+                    if cur_item.key == key:
+                        cur_item.value = None
+                    cur_item = cur_item.next
+        # or if not found
+        else:
+            print("Key not found!")
 
     def retrieve(self, key):
         '''
@@ -90,15 +105,24 @@ class HashTable:
         Returns None if the key is not found.
         Fill this in.
         '''
+        # hash the key
         key_hash = self._hash_mod(key)
+        item = self.storage[key_hash]
 
-        if self.storage[key_hash] is not None:
-            if self.storage[key_hash].key == key:
-                return self.storage[key_hash].next
+        # if index is not empty
+        if item is not None:
+            # and key matches
+            if item.key == key:
+                # return key value
+                return item.value
+            else:
+                cur_item = item.next
+                while cur_item is not None:
+                    if cur_item.key == key:
+                        return cur_item.value
+                    cur_item = cur_item.next
         else:
             return None
-        
-
 
     def resize(self):
         '''
@@ -106,12 +130,27 @@ class HashTable:
         rehash all key/value pairs.
         Fill this in.
         '''
+
+        # save current storage to temp variable
         old_storage = self.storage
+        # increase capacity by multiple of 2
         self.capacity *= 2
+        # set storage to an empty list
         self.storage = [None] * self.capacity
 
-        for i in old_storage:
-            self.insert(i[0], i[1])
+        # insert each item in old storage which is not none
+        for item in old_storage:
+            # if the head is the only item, insert just that
+            if item is not None and item.next is None:
+                self.insert(item.key, item.value)
+            # if there is a LL, iterate over list and insert each one
+            if item is not None and item.next is not None:
+                cur_item = item
+                while cur_item.next is not None:
+                    self.insert(cur_item.key, cur_item.value)
+                    cur_item = cur_item.next
+                # insert the last node in LL
+                self.insert(cur_item.key, cur_item.value)
 
 
 if __name__ == "__main__":
